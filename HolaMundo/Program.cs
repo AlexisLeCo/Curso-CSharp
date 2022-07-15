29d7f1f39e0eff1814107797aa8cf5efa6fc9b6f
Alexis Ledesma <alexisledesma31@hotmail.com>
2022-07-14 23:41:06 -0500

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int num1 = 13;
+         int num2 = 5;
+         int sum = num1 + num2;
+
+         double d1 = 3.2;
+         double d2 = 1.337;
+         double sumad = d1 + num2;
+         Console.WriteLine(" La suma de " + d1 + " más " + num2 + " da como resultado " + sumad);
+         Console.ReadLine();
+        }
+    }
+}
